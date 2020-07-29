aab99480f4398d1c015fa884213eee65686003ed
spdevapp <spdevapp@joara.com>
2020-07-29 14:51:55 +0900

    위치 접근 권한 허용 및 현재 위치 표시

--- Leo/ViewController.swift
+++ Leo/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 import NMapsMap
+import CoreLocation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var naverMapView: NMFNaverMapView!
+
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        initLocation()
         initMapOption()
     }
     
+    func initLocation() {
+        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+
+    }
     func initMapOption() {
         
         naverMapView.mapView.addOptionDelegate(delegate: self)
@@ -28,6 +40,13 @@ class ViewController: UIViewController {
         naverMapView.mapView.setLayerGroup(NMF_LAYER_GROUP_BICYCLE, isEnabled: true)
         naverMapView.mapView.isNightModeEnabled = true
         
+//        naverMapView.showCompass = true
+//        naverMapView.showScaleBar = true
+//        naverMapView.showZoomControls = true
+//        naverMapView.showLocationButton = true
+        naverMapView.mapView.positionMode = .compass
+        
+
     }
 
 
@@ -38,6 +57,25 @@ extension ViewController: NMFMapViewOptionDelegate {
     func mapViewOptionChanged(_ mapView: NMFMapView) {
         
         print("mapViewOptionChanged")
+
     }
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        print("status: \(status.rawValue)")
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let value: CLLocationCoordinate2D = manager.location!.coordinate
+        print("value = \(value.latitude) \(value.longitude)")
+        
+        let position = NMFCameraPosition(NMGLatLng(lat: value.latitude, lng: value.longitude), zoom: 14, tilt: 0, heading: 0)
+        
+        naverMapView.mapView.moveCamera(NMFCameraUpdate(position: position))
+    }
+}
