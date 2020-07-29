b27156d2b2bf5099eb4c2f716364acdd3c8dad72
spdevapp <spdevapp@joara.com>
2020-07-29 12:02:35 +0900

    StoryBoard를 활용하여 NMFNaverMapView 객체를 추가, 기본 옵션을 설정하고 지도 표시하기

--- Leo/ViewController.swift
+++ Leo/ViewController.swift
@@ -11,14 +11,33 @@ import NMapsMap
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var naverMapView: NMFNaverMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let mapView = NMFMapView(frame: view.frame)
-        view.addSubview(mapView)
+        initMapOption()
+    }
+    
+    func initMapOption() {
+        
+        naverMapView.mapView.addOptionDelegate(delegate: self)
+
+        naverMapView.mapView.mapType = .terrain
+        naverMapView.mapView.setLayerGroup(NMF_LAYER_GROUP_BICYCLE, isEnabled: true)
+        naverMapView.mapView.isNightModeEnabled = true
+        
     }
 
 
+}
+
+extension ViewController: NMFMapViewOptionDelegate {
+    
+    func mapViewOptionChanged(_ mapView: NMFMapView) {
+        
+        print("mapViewOptionChanged")
+    }
 }
 
